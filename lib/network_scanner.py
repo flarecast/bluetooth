eb60af551af6048e25403a84b23a0eedc55a85c9
JoRod94 <joao.rod94@gmail.com>
2016-03-15 20:15:42 +0000

    Ready for Demo

--- lib/network_scanner.py
+++ lib/network_scanner.py
@@ -2,6 +2,7 @@ from threading import Thread
 from listener import Listener
 import bluetooth
 import time
+import pickle
 
 class NetworkScanner(Thread):
     def __init__(self, msg):
@@ -10,13 +11,15 @@ class NetworkScanner(Thread):
         self.msg = msg
 
     def broadcast(self):
-        nearby_devices = scan_network
+        nearby_devices = self.scan_network()
         for addr in nearby_devices:
-            if addr not in self.emmitted_devices:
-                emit(self.msg, addr)
-                self.emitted_devices.insert(addr)
+            if addr not in self.emitted_devices:
+                self.emit(self.msg, addr)
+                self.emitted_devices.append(addr)
+            else:
+                print("ALREADY SENT TO: "+addr)
 
-    def emit(msg, addr):
+    def emit(self,msg, addr):
         NetworkScanner.Emitter(msg, addr).start();
 
     def scan_network(self):
@@ -24,7 +27,7 @@ class NetworkScanner(Thread):
 
     def run(self):
         start = time.time()
-        while start + self.msg.insistence < time.time():
+        while start + self.msg.insistence >  time.time():
             self.broadcast()
 
     class Emitter(Thread):
@@ -32,10 +35,17 @@ class NetworkScanner(Thread):
             Thread.__init__(self)
             self.msg = msg
             self.addr = addr
-            self.sock = bluetooth.BluetoothSocket(RFCOMM)
+            self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
         def run(self):
-            sock.connect((self.addr, Listener.DEFAULT_PORT))
-            sock.send(self.msg)
-            sock.close()
+            try:
+                self.sock.connect((self.addr, Listener.DEFAULT_PORT))
+                print("SENDING: "+self.addr)
+                self.sock.send(pickle.dumps(self.msg))
+                self.sock.close()
+            except Exception as e:
+                self.sock.close()
+                print(self.addr + " :: " + str(e))
+                print("IGNORED DEVICE: "+self.addr)
+
 
