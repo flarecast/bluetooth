d359c69d0520b6aeb0bf97bc9c76a75dd375286c
JoRod94 <joao.rod94@gmail.com>
2016-03-28 18:12:10 +0100

    Added repeated message control

--- lib/bluetooth_plugin.py
+++ lib/bluetooth_plugin.py
@@ -1,6 +1,8 @@
 from connection_plugin import ConnectionPlugin
 from listener import Listener
 from network_scanner import NetworkScanner
+import time
+import thread
 
 class BluetoothPlugin(ConnectionPlugin):
     device_blacklist = []
@@ -9,11 +11,23 @@ class BluetoothPlugin(ConnectionPlugin):
         super().__init__()
         self.listener = Listener()
         self.addr = self.__get_bluetooth_addr()
+        self.sent_message_ids = []
+        self.last_removal = time.time()
+
 
     # API method
     def broadcast(self, msg):
-        print("BROADCASTING")
-        NetworkScanner(msg, self).start()
+
+        #Periodic message id removal
+        if(time.time() > self.last_removal+1800):
+            self.last_removal = time.time()
+            thread.start_new_thread(self.remove_old_message_ids)
+
+        #Only sends if not repeated
+        if((msg.full_id(), msg.timestamp+msg.lifetime) not in self.sent_message_ids):
+            self.sent_message_ids.append((msg.full_id(), msg.timestamp + msg.lifetime))
+            print("BROADCASTING")
+            NetworkScanner(msg, self).start()
 
     # API method
     def run(self):
@@ -28,3 +42,13 @@ class BluetoothPlugin(ConnectionPlugin):
             addr = file.read()
         print(addr)
         return addr
+
+        # TODO: return bluetooth address
+        return "12345"
+
+    def remove_old_message_ids(self):
+        curr_time = time.time()
+        for (msg_id, endtime) in self.sent_message_ids:
+            if endtime < curr_time:
+                self.sent_message_ids.remove((msg_id, endtime))
+
